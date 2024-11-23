b8e422cc27e70f5194366c04f247d26fc0026866
sankalpdixit <68779208+sankalpdixit@users.noreply.github.com>
2024-11-23 21:09:54 +0530

    Add files via upload

---
+++ src/bloodbank/Bloodbank.java
@@ -0,0 +1,15 @@
+
+package bloodbank;
+
+
+public class Bloodbank {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        main m=new main();
+        m.setVisible(true);
+    }
+    
+}
